7bfd53db913425a27872a2b080e16fd39aa61c78
tamoorkayani <tamoorkayani@gmail.com>
2018-12-04 21:47:10 +0500

    Update
    Update

--- Contents/Survey/js/eat.js
+++ Contents/Survey/js/eat.js
@@ -13,7 +13,7 @@ $(document).click(function(event) {
 });*/
 //-----------------------------------------------------
 
-
+var field;
 function LanguageChange(lang)
 {
     if(lang === "english")
@@ -65,59 +65,19 @@ $(document).ready(function(){
     //$("$list1").show();
 
   });
-  //$("#form-submit").click(function () {
-    //ShowPopup();
-
-    //$("$list1").show();
-
-  //});
   
+  
+  $("input[type=text], input[type=email]").click(function(){
+    ShowKeyboard();
+    fieldID=$(this).attr('id');
+    field=document.getElementById(fieldID);
+  });
+  $("#ConfirmInput").click(function(){
+    HideKeyboard();
+  });
 
   $("#form-submit").click(function( e ) {
 
-    /*if($("#name").val()== "")
-    {
-      alert("Please enter your name.");
-      $("#name").focus();
-      return false;
-    }
-    $name=$("#name").val();
-    
-    var nameReg = /^[a-zA-Z ]+$/;
-    if( !nameReg.test( $name ) ) {
-    alert("Please enter a valid Name.");
-    $("#name").val("");
-    $("#name").focus();
-    return false;
-    }*/
-
-    
-    /*if($("#company").val()=="")
-    {
-      alert("Please enter Company.");
-      $("#question").focus();
-      return false;
-    }
-    if($("#city").val()=="")
-    {
-      alert("Please enter city.");
-      $("#question").focus();
-      return false;
-    }*/
-
-    /*var radioValue = $('input:radio[name=demo]:checked').val();
-    //alert(radioValue);
-    if (radioValue==null) {
-      alert("Please Select an option.");
-      $("#demo").focus();
-      return false;
-    }
-    if (radioValue=='Other' &&  $("#other-text").val()=="") {
-      alert("Please specify others.");
-      $("#other-text").focus();
-      return false;
-    }
-*/
     var radioValue2 = $('input:radio[name=visit]:checked').val();
     //alert(radioValue);
     if (radioValue2==null) {
@@ -132,37 +92,7 @@ $(document).ready(function(){
       return false;
     }
     ShowPopup();
-    /*if($("#email").val()== "")
-    {
-      alert("Please enter a valid email address.");
-      $("#email").focus();
-      return false;
-    }
-    
-    $email=$("#email").val();
-    
-    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-    if( !emailReg.test( $email ) ) {
-      alert("Please enter a valid email address.");
-      $("#email").val("");
-      $("#email").focus();
-      return false;
-    }*/
-    /*if($("#properties").val()=="")
-    {
-      alert("Please enter properties.");
-      $("#question").focus();
-      return false;
-    }
-    $properties=$("#properties").val();
-    var nameReg = /^[0-9 ]+$/;
-    if( !nameReg.test( $properties ) ) {
-      alert("Please enter a number.");
-      $("#properties").val("");
-      $("#properties").focus();
-      return false;
-    }*/
-  
+   
     });
   $("#form-submit1").click(function(){
     if($("#email").val()== "")
@@ -201,6 +131,7 @@ $(document).ready(function(){
                   $("#contactForm")[0].reset();
                   $('#form-success').show();
                   $( "#form-success" ).delay( 10000 ).fadeOut( 400 );
+                  HideKeyboard();
                   //setTimeout(hideSuccessMessage(), 20000);
 
                 }
@@ -227,7 +158,7 @@ function ShowPopup(src){
   var dialogLeft = (maskWidth/2) - ($('#dialog-box1').width()/2); 
   
   // assign values to the overlay and dialog box
-  $('#dialog-overlay1').css({height:maskHeight, width:maskWidth}).show();
+  //$('#dialog-overlay1').css({height:maskHeight, width:maskWidth}).show();
   $('#dialog-box1').css({top:dialogTop, left:dialogLeft}).show();
   
 
@@ -251,4 +182,53 @@ function ShowPopupARS(src){
     $('#dialog-box').css({top:dialogTop, left:dialogLeft}).show();
 
     document.getElementById('dialog-box').innerHTML = '<a href="#" class="button">Close</a><div class="dialog-content"><div id="dialog-message"><img width="800" src="'+ src +'"/></div></div>';
+}
+
+
+function ShowKeyboard()
+{
+    // 사진이 촬영되기 전에 클릭시 무시
+    //if (isPhotoTaken == false)
+      //  return;
+    
+    // 키보드 보이기
+    document.getElementById("keyboard").style.display = "block";
+    
+    // 메일 전송 안내 스피치
+    //if (isRobot)
+    //{
+        //window.external.PlaySpeech(speechJsonObj["email"][c_language]);
+    //}
+}
+
+/* 키보드 숨김 */
+function HideKeyboard()
+{
+    // 키보드 숨김
+    document.getElementById("keyboard").style.display = "none";
+    
+    // 메일 주소 초기화
+    message = "";
+    document.getElementById("address").innerHTML = message;
+}
+
+/* 메일 주소 입력 */
+var message = "";
+function keyboard(strPara)
+{
+  if (strPara == "bs")
+    {
+    message = message.slice(0, -1);
+  }
+  
+  else
+    {
+    message += strPara;
+  }
+
+  document.getElementById("address").innerHTML = message;
+  field.value=message;
+}
+function ConfirmInput(){
+
 }
